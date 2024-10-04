bf05474f230af679c1dd18fee28c25d39442684d
Sean Mclaughlin <smclaughlin@riptidetech.com>
2024-10-04 14:05:47 -0400

    pucking farentheses

--- hello.go
+++ hello.go
@@ -1,10 +1,10 @@
 package main
 
-import {
+import (
 	"fmt"
 	"github.com/mclaughco/hello/morestrings"
-}
+)
 
 func main() {
-	fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
